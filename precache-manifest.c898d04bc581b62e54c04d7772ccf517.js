c2549a5f32608619cd3c7a3f8cd188d9981d8d2f
sksharma720000 <sk_sharma72000@hotmail.com>
2021-07-17 19:21:26 +0545

    first commit

---
+++ precache-manifest.c898d04bc581b62e54c04d7772ccf517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38aca07ae0d09dc43ad5efd78fe15d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9bc2a653a6ad60c67e",
+    "url": "/static/css/main.f60e2692.chunk.css"
+  },
+  {
+    "revision": "908ccbadb1eeab4075a1",
+    "url": "/static/js/2.2415bec4.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.2415bec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9bc2a653a6ad60c67e",
+    "url": "/static/js/main.7eba9588.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/Logo.5d5d9eef.svg"
+  }
+]);
